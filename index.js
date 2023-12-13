1d8593e8b57b906a916ee2644b85c14870ac3f87
Jillo Wario <willywario0@gmail.com>
2023-12-13 11:27:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Wario",
+  streetAddress: "Ongata Rongai",
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  const newEmployee = { ...employee, [key]: value };
+  return newEmployee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
